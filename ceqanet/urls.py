258810eef622511cef668919801b1bf9339282ca
Eric Lehmer <elehmer@berkeley.edu>
2016-10-14 09:56:45 -0700

    Base URLs imported from Django 1.5

--- ceqanet/urls.py
+++ ceqanet/urls.py
@@ -12,9 +12,23 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^',include('ceqanet.app.urls')),
+    # Examples:
+    # url(r'^$', 'ceqa.views.home', name='home'),
+    # url(r'^ceqa/', include('ceqa.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-]
+    # registration app urls
+    url(r'^accounts/',include('registration.backends.default.urls')),
+)
